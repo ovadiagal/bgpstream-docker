e7a825a96a0ee81811123cb6944d4fdfa5e81e3d
Gal Ovadia <gal.ovadia@gatech.edu>
2024-04-16 13:04:03 -0400

    Update pybgpstream-demo.py

--- pybgpstream-demo.py
+++ pybgpstream-demo.py
@@ -5,9 +5,8 @@ import pybgpstream
 
 stream = pybgpstream.BGPStream(
     # Consider this time interval:
-    # Sat, 01 Aug 2015 7:50:00 GMT -  08:10:00 GMT
-    from_time="2015-08-01 07:50:00", until_time="2015-08-01 08:10:00",
-    collectors=["rrc00"],
+    from_time="2024-02-10 07:50:00", until_time="2024-02-10 08:10:00",
+    collectors=["rrc01"],
     record_type="ribs",
 )
 
